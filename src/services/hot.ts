82109e10a98d7018f0c543b099599dcf9737660d
xienda <2847903595@qq.com>
2025-01-09 23:24:32 +0800

    feat:热门推荐类型定义

--- src/services/hot.ts
+++ src/services/hot.ts
@@ -1,11 +1,12 @@
 import type { PageParams } from "@/types/global"
+import type { HotResult } from "@/types/hot"
 import { http } from "@/utils/http"
 
 // https://pcapi-xiaotuxian-front-devtest.itheima.net
 // 通用热门推荐类型
 type HotParams = PageParams & { subType?: string }
 export const getHotRecommendAPI = (url: string, data?: HotParams) => {
-    return http({
+    return http<HotResult>({
         method: 'GET',
         url,
         data
