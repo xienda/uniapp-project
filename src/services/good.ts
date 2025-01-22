c8ff3dd44fe0fe7135deaa36463c48b97b9a4b86
xienda <2847903595@qq.com>
2025-01-22 10:59:44 +0800

    feat:商品详情页面渲染

--- src/services/good.ts
+++ src/services/good.ts
@@ -1,8 +1,9 @@
+import type { GoodsResult } from "@/types/goods"
 import { http } from "@/utils/http"
 
 // 商品详情 id 商品id
 export const getGoodsByIdAPI = (id: string) => {
-    return http({
+    return http<GoodsResult>({
         method: 'GET',
         url: '/goods',
         data: {
