59409b96eeaed5db3129e9367965efaac9a5a51e
xienda <2847903595@qq.com>
2025-02-08 22:09:42 +0800

    feat:地址模块-新建地址

--- src/pagesMember/address-form/address-form.vue
+++ src/pagesMember/address-form/address-form.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { postMemberAddressAPI } from '@/services/address'
 
 // 表单数据
 const form = ref({
@@ -13,7 +14,6 @@ const form = ref({
   isDefault: 0, // 默认地址，1为是，0为否
 })
 
-// 获取页面参数
 
 const query = defineProps<{
   id?: string
@@ -21,6 +21,40 @@ const query = defineProps<{
 
 // 动态设置标题
 uni.setNavigationBarTitle({ title: query.id ? '修改地址' : '新建地址' })
+
+// 收集所在地区
+const onRegionChange: UniHelper.RegionPickerOnChange = (ev) => {
+  // 省市区（前端展示)
+  form.value.fullLocation = ev.detail.value.join(' ')
+  // 省市区（后端参数）
+  const [provinceCode, cityCode, countyCode] = ev.detail.code!
+  // form.value.provinceCode = provinceCode
+  Object.assign(form.value, {
+    provinceCode, cityCode, countyCode
+  })
+}
+
+// 收集是否默认收货地址
+const onSwitchChange: UniHelper.SwitchOnChange = (ev) => {
+  form.value.isDefault = ev.detail.value ? 1 : 0
+}
+
+// 提交表单
+
+const onsubmit = async () => {
+  // 新建地址请求
+  const res = await postMemberAddressAPI(form.value)
+  // 成功提示
+  uni.showToast({ icon: 'success', title: '添加成功' })
+  // 返回上一页
+  setTimeout(() => {
+    uni.navigateBack()
+  }, 400)
+
+
+}
+
+
 
 </script>
 
@@ -30,31 +64,31 @@ uni.setNavigationBarTitle({ title: query.id ? '修改地址' : '新建地址' })
       <!-- 表单内容 -->
       <view class="form-item">
         <text class="label">收货人</text>
-        <input class="input" placeholder="请填写收货人姓名" value="" />
+        <input class="input" placeholder="请填写收货人姓名" v-model="form.receiver" />
       </view>
       <view class="form-item">
         <text class="label">手机号码</text>
-        <input class="input" placeholder="请填写收货人手机号码" value="" />
+        <input class="input" placeholder="请填写收货人手机号码" v-model="form.contact" />
       </view>
       <view class="form-item">
         <text class="label">所在地区</text>
-        <picker class="picker" mode="region" value="">
-          <view v-if="false">广东省 广州市 天河区</view>
+        <picker @change="onRegionChange" class="picker" mode="region" :value="form.fullLocation.split(' ')">
+          <view v-if="form.fullLocation">{{ form.fullLocation }}</view>
           <view v-else class="placeholder">请选择省/市/区(县)</view>
         </picker>
       </view>
       <view class="form-item">
         <text class="label">详细地址</text>
-        <input class="input" placeholder="街道、楼牌号等信息" value="" />
+        <input class="input" placeholder="街道、楼牌号等信息" v-model="form.address" />
       </view>
       <view class="form-item">
         <label class="label">设为默认地址</label>
-        <switch class="switch" color="#27ba9b" :checked="true" />
+        <switch @change="onSwitchChange" class="switch" color="#27ba9b" :checked="form.isDefault === 1" />
       </view>
     </form>
   </view>
   <!-- 提交按钮 -->
-  <button class="button">保存并使用</button>
+  <button @tap="onsubmit" class="button">保存并使用</button>
 </template>
 
 <style lang="scss">
---
+++ src/services/address.ts
@@ -0,0 +1,13 @@
+
+
+// /member/address
+import type { AddressParams } from '@/types/address'
+import { http } from "@/utils/http"
+
+export const postMemberAddressAPI = (data: AddressParams) => {
+    return http({
+        method: 'POST',
+        url: '/member/address',
+        data,
+    })
+}
